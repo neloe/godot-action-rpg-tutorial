59ce1dc6196f7df1d026011767884eed33d68deb
Nathan Eloe <powerofazure@gmail.com>
2020-12-27 09:55:23 -0600

    Adding basic physics: smooth move exlax (vid 2)
    Note: multiplying by delta should NOT be in clamped, fixed in vid 3

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,11 @@ extends KinematicBody2D
 
 
 var velocity = Vector2.ZERO
-var speed = 4
+
+const ACCELERATION = 10
+const MAX_SPEED = 100
+const FRICTION = 10
+
 
 # Called when the node enters the scene tree for the first time.
 	
@@ -22,10 +26,13 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta) 
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
+	print(velocity)
 	move_and_collide(velocity)
