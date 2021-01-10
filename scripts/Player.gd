cc061251a9f748975fa7d6ec44df6aabfc2487f2
Nathan Eloe <powerofazure@gmail.com>
2021-01-10 08:51:50 -0600

    Add attack animation and state machine (vid 9)

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
+# Neat!
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
 
+var state = MOVE
 var velocity = Vector2.ZERO
 
 const ACCELERATION = 500
@@ -16,7 +23,19 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get('parameters/playback')
 # Called when the node enters the scene tree for the first time.
 	
+func _ready():
+	animationTree.active = true
+	
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	# Bad way
 	#if Input.is_action_pressed("ui_right"):
 	#	velocity.x = speed
@@ -37,6 +56,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set('parameters/Idle/blend_position', input_vector)
 		animationTree.set('parameters/Run/blend_position', input_vector)
+		animationTree.set('parameters/Attack/blend_position', input_vector)
 		animationState.travel('Run')
 		#if input_vector.x > 0:
 		#	animationPlayer.play("RunRight")
@@ -52,3 +72,13 @@ func _physics_process(delta):
 	
 	# this returns the new velocity
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel('Attack')
+
+func attack_animation_finished():
+	state = MOVE
